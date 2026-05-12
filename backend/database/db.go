55865aee48c3b2c5154de7127dee21e702cbd014
Pranav1703 <pranavsai2005@gmail.com>
2026-05-12 13:17:20 +0530

    add timeout for db conn

--- backend/database/db.go
+++ backend/database/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -11,14 +12,27 @@ import (
 var DB *pgx.Conn
 
 func initDb(){
-	ctx := context.Background()
-	DB_URL := os.Getenv("DB_URL")
-	conn, err := pgx.Connect(ctx, DB_URL)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+	conn, err := pgx.Connect(ctx, dbURL)
 	if err != nil {
 		log.Fatalln("Failed to connect to DB.\n",err)
 	}
 
-	conn.Exec(ctx,"CREATE EXTENSION IF NOT EXISTS vector")
+	if err :=conn.Ping(ctx); err != nil {
+		log.Fatalln(err)
+	}
+
+	_, err = conn.Exec(ctx,"CREATE EXTENSION IF NOT EXISTS vector")
+	if err != nil {
+		log.Fatalf("Failed to create extension: %v\n", err)
+	}
+	log.Println("Successfully connected to DB")
 	DB = conn
 }
 
